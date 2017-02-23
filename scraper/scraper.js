fef570be77ae90cf99c32ec548d499263b07cc31
Derek <namjjd@gmail.com>
2017-02-23 06:40:10 +0630

    Tidied up

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -11,6 +11,9 @@ var urlsPath = '../config/urls.config';
 var downloadConfigPath = '../config/url.config';
 var downloadPath = '';
 
+// Configuration
+var audioSavePath = './clips';
+
 // Results
 var results = [];
 
@@ -40,46 +43,44 @@ function startScrape(url) {
     fetch(url, function(err, meta, body) {
         var $ = cheerio.load(body);
 
-        var defResults = new Array();
-        var pathResults = new Array();
-        var unicodeResults = new Array();
+        var defResults = [];
+        var pathResults = [];
+        var unicodeResults = [];
 
         var getUnicode = $('.highlight-block-right-wide article ol li span span');
         getUnicode.each(function(i, line) {
-            var line = line.children[0].data;
-            unicodeResults.push(line);
-            console.log(line);
+            var gline = line.children[0].data;
+            unicodeResults.push(gline);
         });
 
         var defn = $('.highlight-block-right-wide article ol li a');
         defn.each(function(i, line) {
             try{
-                if(line.next.data && (i % 2 == 0)){
-                   var data = line.next.data.replace(/[\x00-\x1F\x7F-\x9F]/g, "");;
+                if(line.next.data && (i % 2 === 0)){
+                   var data = line.next.data.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
                    defResults.push(data);
                 }
             } catch (e){}
         });
-
         var aud = $('.highlight-block-right-wide article ol li a audio');
         aud.each(function(i, line) {
             try{
                 var path = line.attribs.src;
-    
+
                 var url = downloadPath + path;
 
                 var options = {
-                    directory: './clips/'
-                }
+                    directory: audioSavePath
+                };
 
-                pathResults.push(path);
+                pathResults.push(getRelativePath(path));
 
-                download(url, options, function(err) {
+                /*download(url, options, function(err) {
                     if (err) {
                         throw err;
                     }
                     console.log("Path " + url + " has completed.");
-                });
+                });*/
             } catch (e){}
         });
 
@@ -89,13 +90,17 @@ function startScrape(url) {
 
 function consolidateResults(unicodeResults, defResults, pathResults) {
     unicodeResults.forEach(function (result, i) {
-        console.log(defResults + i);
         var obj = {
             'unicode' : result,
             'definition' : defResults[i],
             'path' : pathResults[i]
-        }
+        };
         results.push(obj);
     });
     console.log(results);
+}
+
+function getRelativePath(path) {
+    var segments = path.split('/');
+    return segments[segments.length - 1];
 }
