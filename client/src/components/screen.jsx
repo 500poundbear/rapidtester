45bb0b1529faf287b27e3e52f6b51e4f49567c18
Derek <namjjd@gmail.com>
2017-02-23 15:42:33 +0630

    Improved screen component to receive props

--- client/src/components/screen.jsx
+++ client/src/components/screen.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 export default React.createClass({
+  getDescription: function() {
+    return this.props.description || "Missing description ba ba ba";
+  },
+  getRomanisation: function() {
+    return this.props.romanisation || 'Ba1';
+  },
+  getCharacters: function() {
+    return this.props.characters || 'ဘ';
+  },
   render: function() {
     let divStyle = {
       backgroundColor: '#eeeeee',
@@ -14,14 +23,25 @@ export default React.createClass({
       color: '#63700B',
       marginTop: '100px'
     };
+    let paraStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      fontSize: '25px',
+      marginLeft: '20px',
+      marginRight: '20px',
+      color: '#444444'
+    }
     return <div>
       <h2 style={{color: '#C1CF63', display: 'flex', justifyContent: 'center', marginTop:'10px'}}>
         If it were easy, &nbsp;<u>everyone</u> &nbsp;would do it. Never back down!
       </h2>
       <div style={divStyle}>
         <p>&nbsp;</p>
-        <div style={superBig}>ဘား</div>
-        <h1 style={{justifyContent: 'center', display: 'flex', marginTop: '160px'}}>Ha3</h1>
+        <div style={superBig}>{this.getCharacters()}</div>
+        <h1 style={{justifyContent: 'center', display: 'flex', marginTop: '160px', color: '#222222'}}>
+          {this.getRomanisation()}
+        </h1>
+        <p style={paraStyle}>{this.getDescription()}</p>
       </div>
     </div>;
   }
